05778c72913f58b0964611583ab433ebad34e2dd
RegiMaria <regimaria015@gmail.com>
2026-07-06 14:56:26 -0300

    chore: reexporta classes de extractors no __init__.py

---
+++ src/olist_etl/extractors/__init__.py
@@ -0,0 +1,5 @@
+from .base import BaseExtractor
+from .csv_extractor import CsvExtractor
+from .parquet_extractor import ParquetExtractor
+from .json_extractor import JsonExtractor
+from .api_extractor import ApiExtractor
